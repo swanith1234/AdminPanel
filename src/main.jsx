e22cd9b0977b2abd3e5ff1749af9bc96a37cfc83
swanith1234 <swanithpidugu@gmail.com>
2024-07-09 17:14:43 +0530

    404.html

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <React.StrictMode>
+    <React.StrictMode basename="/AdminPanel/">
       <App />
     </React.StrictMode>
   </BrowserRouter>
